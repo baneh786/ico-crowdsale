471c368335026b598e66035a3b79ff4fac4d69c5
BANEHAR SK <59317408+baneh786@users.noreply.github.com>
2021-01-22 07:57:34 +0000

    rubigoldcoin
    This is my new crypto currencys by erc 20 testmet.recenly coming soon mainnet ...
    ,,rubigoldcoin,,  token..rubigoldcoin.

--- contracts/misc/Migrations.sol
+++ contracts/misc/Migrations.sol
@@ -1,23 +1,53 @@
-pragma solidity ^0.4.23;
 
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
 
-  constructor() public {
-    owner = msg.sender;
-  }
+// SPDX-License-Identifier: GPL-3.0
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
+pragma solidity >=0.7.0 <0.8.0;
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+/**
+* @title Owner
+* @dev Set & change owner
+*/
+contract Owner {
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+address private owner;
+
+// event for EVM logging
+event OwnerSet(address indexed oldOwner, address indexed newOwner);
+
+// modifier to check if caller is owner
+modifier isOwner() {
+// If the first argument of 'require' evaluates to 'false', execution terminates and all
+// changes to the state and to Ether balances are reverted.
+// This used to consume all gas in old EVM versions, but not anymore.
+// It is often a good idea to use 'require' to check if functions are called correctly.
+// As a second argument, you can also provid.e an explanation about what went wrong.
+require(msg.sender == owner, "Caller is not owner");
+_;
+}
+
+/**
+* @dev Set contract deployer as owner
+*/
+constructor() {
+owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+emit OwnerSet(address(0), owner);
+}
+
+/**
+* @dev Change owner
+* @param newOwner address of new owner
+*/
+function changeOwner(address newOwner) public isOwner {
+emit OwnerSet(owner, newOwner);
+owner = newOwner;
+}
+
+/**
+* @dev Return owner address 
+* @return address of owner
+*/
+function getOwner() external view returns (address) {
+return owner;
+}
 }
